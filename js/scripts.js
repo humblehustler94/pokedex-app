85fd81a0e44533421b1058deac047d41bd0039df
humblehustler94 <flores.itzel94@gmail.com>
2024-08-02 14:06:34 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,8 @@ let pokemonList = [
 
     { name: 'Bulbasaur', height: '0.7', types:['grass', 'posion']},
     { name: 'Charamander', height: '0.6', types: 'fire'},
-    { name: 'Squirtle', height: '0.5', types:'water'},
-    { name: 'Caterpie', height: '0.3', types:'bug'},
+    { name: 'Squirtle', height: '0.5', types: 'water'},
+    { name: 'Caterpie', height: '0.3', types: 'bug'},
     { name: 'Butterface', height: '1.1', types:['bug','flying']},
 
 ]
