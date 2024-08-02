73ce4f6bc918b89bc55fe6a80acde6666c54fb3a
humblehustler94 <flores.itzel94@gmail.com>
2024-08-02 14:00:08 -0700

    Update scripts.js
    added pokemonList with a minimum of 5 Pokémon characters.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,9 @@
-alert('Hello World');
+let pokemonList = [
 
-let favoriteFood = 'sushi';
-document.write(favoriteFood);
+    { name: 'Bulbasaur', height: '0.7', types:['grass', 'posion']},
+    { name: 'Charamander', height: '0.6', types: 'fire'},
+    { name: 'Squirtle', height: '0.5', types:'water'},
+    { name: 'Caterpie', height: '0.3', types:'bug'},
+    { name: 'Butterface', height: '1.1', types:['bug','flying']},
+
+]
