9df94274d7e67e05fefc9a9fe5767a9305de19d2
humblehustler94 <flores.itzel94@gmail.com>
2024-08-06 14:56:25 -0700

    Summary
    add minor style to app.
    wrote my for code if-else statement.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,15 @@
 let pokemonList = [
 
-    { name: 'Bulbasaur', height: '0.7', types:['grass', 'posion']},
-    { name: 'Charamander', height: '0.6', types: 'fire'},
-    { name: 'Squirtle', height: '0.5', types: 'water'},
-    { name: 'Caterpie', height: '0.3', types: 'bug'},
-    { name: 'Butterface', height: '1.1', types:['bug','flying']},
-
+    { name: 'Bulbasaur', height: '0.7', weight: '6.9', types: ['grass', 'posion'] },
+    { name: 'Charizard', height: '1.7', weight: '90.5', types: ['fire', 'flying'] },
+    { name: 'Squirtle', height: '0.5', weight: '9', types: 'water' },
 ]
+
+for (let i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i].height > 1.5) {
+        document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')' + ' - Wow, that\'s big!' + '<br>');
+    } else {
+        document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')' + '<br>');
+    }
+} // this code executes task 2.3 
+
