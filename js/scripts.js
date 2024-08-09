58fb1c9c2d6246915c8b8698e8f9a5219b6c7d75
humblehustler94 <flores.itzel94@gmail.com>
2024-08-09 13:01:35 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,13 @@
-let pokemonRepository = (function() {
-    // Define the pokemonList array inside the IIFE
-    let pokemonList = [
-      { name: 'Bulbasaur', height: '0.7', weight: '6.9', types: ['grass', 'poison'] },
-      { name: 'Charizard', height: '1.7', weight: '90.5', types: ['fire', 'flying'] },
-      { name: 'Squirtle', height: '0.5', weight: '9', types: ['water'] },
-    ];
-  
-    // Define the getAll function
-    function getAll() {
-      return pokemonList;
-    }
-  
-    // Define the add function
-    function add(pokemon) {
-      pokemonList.push(pokemon);
-    }
-  
-    // Return an object with the public functions
-    return {
-      getAll: getAll,
-      add: add
-    };
-  })();
-  
-  // Use the getAll method to retrieve and log the list of Pokémon
-  pokemonRepository.getAll().forEach(function(pokemon) {
-    console.log(pokemon);
-  });
-  
+let pokemonList = [
+    {name: 'Bulbasaur', height: '0.7', types: ['grass', 'poison']},
+    {name: 'Charizard', height: '1.7', types: ['fire', 'flying']},
+    {name: 'Squirtle', height: '0.5', types:['water']},
+];
+
+for (let i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i].height > 1.5) {
+        document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')' + ' - Wow, that\'s big!' + '<br>');
+    } else {
+        document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')' + '<br>');
+
+        
