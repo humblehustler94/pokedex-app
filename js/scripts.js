58ead54185f977fefd5a98d4e11fc2e2598d98e8
humblehustler94 <flores.itzel94@gmail.com>
2024-08-09 15:30:55 -0700

    Update scripts.js
    Enhancement: Add validation to add() function and implement findByName() function in pokemonRepository.

--- js/scripts.js
+++ js/scripts.js
@@ -10,20 +10,47 @@ let pokemonRepository = (function () {
     }
 
     function add(pokemon) {
-        if (typeof pokemon === 'object' && 'name' in pokemon && 'height' in pokemon && 'weight' in pokemon && 'types' in pokemon) {
+        if (typeof pokemon === 'object' &&
+            'name' in pokemon && typeof pokemon.name === 'string' &&
+            'height' in pokemon && typeof pokemon.height === 'number' &&
+            'weight' in pokemon && typeof pokemon.weight === 'number' &&
+            'types' in pokemon && Array.isArray(pokemon.types)) {
+
             pokemonList.push(pokemon);
         } else {
             console.log('Invalid Pokémon object');
         }
     }
 
+    function findByName(name) {
+        return pokemonList.filter(pokemon => pokemon.name.toLowerCase() === name.toLowerCase());
+    }
+
     return {
         getAll: getAll,
-        add: add
+        add: add,
+        findByName: findByName
     };
 })();
 
+
+// added new pokemons to the list
+pokemonRepository.add({ name: 'Pikachu', height: 0.4, weight: 6.0, types: ['electric'] });
+
+pokemonRepository.add({ name: 'Caterpie', height: 0.3, weight: 2.9, types: ['bug'] }); // n
+
+
+let foundPokemon = pokemonRepository.findByName('Pikachu');
+console.log(foundPokemon); // This will log the Pikachu object if found
+
+let foundPoke = pokemonRepository.findByName('Caterpie');
+console.log(foundPoke); // This will log the Caterpie object if found
+
+
+// Iterating over the pokemonList using the getAll function and forEach
+
 pokemonRepository.getAll().forEach(pokemon => {
+
     let message = `${pokemon.name} (height: ${pokemon.height})`;
     if (pokemon.height > 1.5) {
         message += " - Wow, that's big!";
