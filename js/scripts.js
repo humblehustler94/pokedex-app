c23cab3e906e0c359beddc705c8088a2dca693fb
humblehustler94 <flores.itzel94@gmail.com>
2024-08-09 13:02:42 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -9,5 +9,6 @@ for (let i = 0; i < pokemonList.length; i++) {
         document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')' + ' - Wow, that\'s big!' + '<br>');
     } else {
         document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')' + '<br>');
+    }
+}
 
-        
