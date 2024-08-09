57cb6a3fa7f81f71d9639a67d3efaecb8617e1c8
humblehustler94 <flores.itzel94@gmail.com>
2024-08-09 13:33:52 -0700

    Update scripts.js
    Refactor: Encapsulate pokemonList in an IIFE and update loop to use pokemonRepository functions.

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,32 @@
-let pokemonList = [
+let pokemonRepository = (function () {
+    let pokemonList = [
+        { name: 'Bulbasaur', height: '0.7', weight: '6.9', types: ['grass', 'poison'] },
+        { name: 'Charizard', height: '1.7', weight: '90.5', types: ['fire', 'flying'] },
+        { name: 'Squirtle', height: '0.5', weight: '9', types: ['water'] },
+    ];
 
-    { name: 'Bulbasaur', height: '0.7', weight: '6.9', types: ['grass', 'posion'] },
-    { name: 'Charizard', height: '1.7', weight: '90.5', types: ['fire', 'flying'] },
-    { name: 'Squirtle', height: '0.5', weight: '9', types: ['water'] },
-];
-
-/*
-for (let i = 0; i < pokemonList.length; i++) {
-    if (pokemonList[i].height > 1.5) {
-        document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')' + ' - Wow, that\'s big!' + '<br>');
-    } else {
-        document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')' + '<br>');
+    function getAll() {
+        return pokemonList;
     }
-}
 
-*/
+    function add(pokemon) {
+        if (typeof pokemon === 'object' && 'name' in pokemon && 'height' in pokemon && 'weight' in pokemon && 'types' in pokemon) {
+            pokemonList.push(pokemon);
+        } else {
+            console.log('Invalid Pokémon object');
+        }
+    }
 
-// this code executed in task 2.3
+    return {
+        getAll: getAll,
+        add: add
+    };
+})();
 
-
-// this line of code executes same as above but with forEach() function.
-pokemonList.forEach(pokemon => { 
+pokemonRepository.getAll().forEach(pokemon => {
     let message = `${pokemon.name} (height: ${pokemon.height})`;
     if (pokemon.height > 1.5) {
         message += " - Wow, that's big!";
     }
     document.write(message + '<br>');
-  });
-
-  
+});
