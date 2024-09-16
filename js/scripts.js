399e997e617847da71313fa2c8f6004d3ce036bd
humblehustler94 <flores.itzel94@gmail.com>
2024-09-16 14:23:18 -0700

    Update scripts.js
    2.6 task completed. added the following codes : function addListItem(pokemon)
    function showDetails, to the return we have addListItem: showDetails:
    add an event listener  to button.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 let pokemonRepository = (function () {
-    let repository = [
+    let pokemonList = [
         {
             name: "Bulbasaur",
             height: 0.7,
@@ -17,53 +17,66 @@ let pokemonRepository = (function () {
         },
     ];
 
-    function add(pokemon) {
-        if (
-            typeof pokemon === "object" &&
-            "name" in pokemon &&
-            "height" in pokemon &&
-            "types" in pokemon
-        ) {
-            repository.push(pokemon);
-        } else {
-            console.log("pokemon is not correct");
-        }
-    }
-    function findByName(name) { // code done in 2.5 task
-        return repository.filter(pokemon => pokemon.name.toLowerCase() === name.toLowerCase());
-    }
     function getAll() {
-        return repository;
-    }
-    function addListItem(pokemon) { // code added in 2.6
-        let pokemonList = document.querySelector(".pokemon-list");
-        let listpokemon = document.createElement("li");
-        let button = document.createElement("button");
-        button.innerText = pokemon.name;
-        button.classList.add("button-class");
-        listpokemon.appendChild(button);
-        pokemonList.appendChild(listpokemon);
+        return pokemonList;
     }
 
-    /* create a new function called showDetails () */
+    function add(pokemon) {
+        if (typeof pokemon === 'object' &&
+            'name' in pokemon && typeof pokemon.name === 'string' &&
+            'height' in pokemon && typeof pokemon.height === 'number' &&
+            'weight' in pokemon && typeof pokemon.weight === 'number' &&
+            'types' in pokemon && Array.isArray(pokemon.types)) {
+
+            pokemonList.push(pokemon);
+        } else {
+            console.log('Invalid Pokémon object');
+        }
+    }
+    // function findByName task 2.5
+    function findByName(name) {
+        return pokemonList.filter(pokemon => pokemon.name.toLowerCase() === name.toLowerCase());
+    }
+    function addListItem(pokemon){ // function addListItem added in 2.6 creates pokemon list w/ containers wrapped on the outside thanks to button-class -css rule.
+        let pokemonList = document.querySelector(".pokemon-list");
+        let listpokemon = document.createElement("li"); // create li elememnt
+        let button = document.createElement("button"); // creates a button
+        // set the button text and class
+        button.innerText = pokemon.name;
+        button.classList.add("button-class"); // targets css rule for style to button.
+        //append the button to the list item, and the list item to the list
+        listpokemon.appendChild(button);
+        pokemonList.appendChild(listpokemon);
+        // add event listener to the button code added as part of 2.6 task.
+        button.addEventListener("click",function() {
+            showDetails(pokemon); // pass the pokemon object to showDetails
+        });
+    }
     function showDetails(pokemon) {
         console.log(pokemon);
     }
 
     return {
-        add: add,
-        findByName: findByName, // code from 2.5
         getAll: getAll,
-        addListItem: addListItem // code added in 2.6
+        add: add,
+        findByName: findByName,// 2.5 task code.
+        addListItem: addListItem, // code added in 2.6 task.
+        showDetails: showDetails // code added in 2.6 task.
     };
 })();
 
-pokemonRepository.add({ name: "Pikachu", height: 0.3, types: ["electric"] });
+
+console.log(pokemonRepository.getAll());
+pokemonRepository.add({ name: 'Pikachu', height: 0.4, weight: 6.0, types: ['electric'] }); // added new pokemons to the list
+
 pokemonRepository.add({ name: 'Caterpie', height: 0.3, weight: 2.9, types: ['bug'] });
 
-
-// added new pokemons to the list
 console.log(pokemonRepository.getAll());
+
+pokemonRepository.getAll().forEach(function (pokemon) {
+    pokemonRepository.addListItem(pokemon);
+
+});
 
 
 //let foundPokemon = pokemonRepository.findByName('Pikachu');
@@ -72,20 +85,15 @@ console.log(pokemonRepository.getAll());
 //let foundPoke = pokemonRepository.findByName('Caterpie');
 //console.log(foundPoke); // This will log the Caterpie object if found
 
-
 // Iterating over the pokemonList using the getAll function and forEach
-/*
-pokemonRepository.getAll().forEach(pokemon => { // 2.5 code
 
-    let message = `${pokemon.name} (height: ${pokemon.height})`;
-    if (pokemon.height > 1.5) {
-        message += " - Wow, that's big!";
-    }
-    document.write(message + '<br>');
+
+/*
+pokemonRepository.getAll().forEach(pokemon => {
+let message = `${pokemon.name} (height: ${pokemon.height})`;
+if (pokemon.height > 1.5) {
+    message += " - Wow, that's big!";
+}
+document.write(message + '<br>');
 });
 */
-
-pokemonRepository.getAll().forEach(function (pokemon) {
-    pokemonRepository.addListItem(pokemon);
-
-});
